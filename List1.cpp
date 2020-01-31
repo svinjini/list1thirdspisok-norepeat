e6bc5658e75d4bf8962e6dd54aa9107178752586
Vera Alexeeva <vera.cortie@gmail.com>
2020-01-31 15:03:06 +0300

    in progress

--- List1.cpp
+++ List1.cpp
@@ -1,5 +1,52 @@
 #ifndef _HEADER_INCLUDED_
 #include "List.h"
 #define _HEADER_INCLUDED_
-
+List::List(){
+		head=nullptr;
+		tail=nullptr;
+	}
+List::List(List& ob){
+		   
+	}
+List::List(List&& ob){
+	   
+	}
+List::~List(){
+		Node* temp=head->next;
+		while(temp!=tail){
+			    head->next=temp->next;
+				deleteNode(temp);
+				temp=head->next;
+			}
+			deleteNode(head);
+	}
+void List::deleteNode(Node* a){
+	    delete a;
+	}
+bool List::isSame(int x){
+	Node* temp=head;
+		while(temp!=tail){
+				if(temp->value=x){
+						return true;
+						break;
+					}
+				temp=temp->next;
+			}
+		return false;
+	}
+List& List::operator +=(int x){
+		Node* temp;
+		if(head==nullptr){
+				head->value=x;
+				head->next=tail;
+			}
+		else{
+			if(isSame(x)!=true){
+				temp=tail;
+				tail=new Node();
+				temp->value=x;
+				temp->next=tail;
+			}
+		}
+	}		
 #endif
--- List1.h
+++ List1.h
@@ -2,8 +2,24 @@
 
 
 class List{
-	List();
-	List( )
-	
-	
+	public:
+		struct Node{
+				Node* next;
+				int value;
+				Node(){
+						next=nullptr;
+						value=0;
+					}
+		};
+		Node* head;
+		Node* tail;
+		List();
+		List(List ob);
+		List(List&& ob);
+		~List();
+		void deleteNode;
+		bool isSame(int x);
+		List& operator +=(int x);
+		friend std::ostream &operator <<(const std::ostream& out, List& ob);
+		friend List& operator |(List& ob);
 };
